953087942dc4f5092361448d307181c9d3c8c17c
sojeongw <sojeongw.official@gmail.com>
2025-05-28 12:06:46 +0900

    4.4. 객체 타입의 호환성

---
+++ section03/src/chapter3.ts
@@ -0,0 +1,58 @@
+// 기본 타입간의 호환성
+let num1: number = 10;
+let num2: 10 = 10;
+num1 = num2;
+
+// 객체 타입간의 호환성: 어떤  객체 타입을 다른 객체 타입으로 취급해도 괜찮은가?
+
+type Animal = {
+  name: string;
+  color: string;
+};
+
+type Dog = {
+  name: string;
+  color: string;
+  breed: string;
+};
+
+let animal: Animal = {
+  name: "기린",
+  color: "yellow",
+};
+
+let dog: Dog = {
+  name: "돌돌이",
+  color: "brown",
+  breed: "진도",
+};
+
+// 프로퍼티를 기준으로 관게를 갖는다.
+animal = dog;
+// dog = animal;
+
+type Book = {
+  name: string;
+  price: number;
+};
+
+type ProgrammingBook = {
+  name: string;
+  price: number;
+  skill: string;
+};
+
+let book: Book;
+let programmingBook: ProgrammingBook = {
+  name: "한 입 크기로 잘라먹는 리액트",
+  price: 33000,
+  skill: "reactjs",
+};
+
+book = programmingBook;
+
+let book2: Book = {
+  name: "한 입 크기로 잘라먹는 리액트",
+  price: 33000,
+  skill: "reactjs",
+};
