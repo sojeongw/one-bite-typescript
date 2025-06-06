22d3d6838c0495c593967f0fc152fc29f8ce7d2e
sojeongw <sojeongw.official@gmail.com>
2025-06-06 13:41:32 +0900

    10.2. 분산적인 조건부 타입

--- section09/src/chapter1.ts
+++ section09/src/chapter1.ts
@@ -30,3 +30,17 @@ function removeSpaces<T>(text: any) {
 
 let result = removeSpaces("hi im winterlood");
 result.toUpperCase();
+
+// 분산적인 조건부 타입
+// 유니언으로 하면 분산적으로 바뀐다.
+let c: StringNumberSwitch<number | string>;
+
+let d: StringNumberSwitch<boolean | number | string>;
+
+// 실용적인 예제
+type Exclude<T, U> = T extends U ? never : T;
+// never가 포함되어 있으면 사라진다.
+type C = Exclude<number | string | boolean, string>;
+
+type Extract<T, U> = T extends U ? T : never;
+type D = Extract<number | string | boolean, string>;
