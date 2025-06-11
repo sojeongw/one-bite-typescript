c68f455a7ec7e3c96ced26e7fce6f136d0169ec2
sojeongw <sojeongw.official@gmail.com>
2025-06-11 15:42:03 +0900

    12. 리액트와 타입스크립트

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,24 +1,51 @@
-import { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect, useReducer } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
+import { Todo } from "./types";
+import TodoItem from "./components/TodoItem";
 
-interface Todo {
-  id: number;
-  content: string;
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: "DELETE"; id: number };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [...state, action.data];
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
 }
 
+export const TodoStateContext = React.createContext<Todo[] | null>(null);
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    dispatch({
+      type: "CREATE",
+      data: {
         id: idRef.current++,
         content: text,
       },
-    ]);
+    });
+  };
+
+  const onClickDelete = (id: number) => {
+    dispatch({
+      type: "DELETE",
+      id: id,
+    });
   };
 
   useEffect(() => {
@@ -28,12 +55,14 @@ function App() {
   return (
     <div className="App">
       <h1>Todo</h1>
-      <Editor onClickAdd={onClickAdd}></Editor>
-      <div>
-        {todos.map((todo) => (
-          <div key={todo.id}>{todo.content}</div>
-        ))}
-      </div>
+      <TodoStateContext.Provider value={todos}>
+        <Editor onClickAdd={onClickAdd}></Editor>
+        <div>
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
+          ))}
+        </div>
+      </TodoStateContext.Provider>
     </div>
   );
 }
